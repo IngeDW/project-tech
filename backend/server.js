6b06c87ee692fb5d41a7d82f6b9d583b6208731e
IngeDW <inge_dewaard@hotmail.com>
2020-03-16 11:04:01 +0100

    Commit voor Peer-review

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,33 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 
-app.get('/', (req, res) => res.render('index.ejs', {title:'Hallo Jaaaade'}));
+app.get('/', (req, res) => res.render('index.ejs', {title:'Hi, please fill in the form!'}));
+
+
+
+
 app.get('/error', (req, res) => res.status(404).render('index.ejs', {title:'Panieek'}));
-
-
-
-
-//app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/about', (req, res) => res.send('Over mij'))
 app.get('/file', (req, res) => res.sendFile('/Users/inge/Documents/• HVA/JAAR 2/TECH/datingapp/static/images/banner6.jpg'))
 app.get('/mp3', (req, res) => res.sendFile('/Users/inge/Documents/• HVA/JAAR 2/TECH/datingapp/static/images/mpvierrr.mp4'))
-
 app.get('*', (req, res) => res.send('404 error not found'))
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+app.post(‘/add-movie', addMovie);
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+function addMovie(req, res) {
+
+  myMovies.push( {
+      id: id;
+      title: req.body.titile,
+      password: req.body.password;
+    } )
+  }
+  res.redirect(‘movie’/ + id);
+}
--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'));
+//public
+app.use(express.static('static'));
+//app.use('/static', express.static(__dirname + '/static));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+
+// get stuurt iets terug
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => res.render('index.ejs', {title:'Hi, please add a movie!'}));
+app.get('/error', (req, res) => res.status(404).render('index.ejs', {title:'Panieek'}));
+
+
 app.get('/about', (req, res) => res.send('Over mij'))
 app.get('/file', (req, res) => res.sendFile('/Users/inge/Documents/• HVA/JAAR 2/TECH/datingapp/static/images/banner6.jpg'))
 app.get('/mp3', (req, res) => res.sendFile('/Users/inge/Documents/• HVA/JAAR 2/TECH/datingapp/static/images/mpvierrr.mp4'))
@@ -18,6 +30,20 @@ app.get('*', (req, res) => res.send('404 error not found'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-let ejs = require('ejs'),
-    people = ['geddy', 'neil', 'alex'],
-    html = ejs.render('<%= people.join(", "); %>', {people: people});
+
+
+//
+app.post(‘/add, addMovie);
+
+// haal info uit form
+
+function addMovie(req, res) {
+
+  myMovies.push( {
+	    id: id,
+	    title: req.body.title,
+	    password: req.body.password
+    })
+  }
+  res.redirect(‘movie’/ + id);
+}
