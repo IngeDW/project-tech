6c2e5e470cece2a9c20520ae483d5a0e03d83a3c
IngeDW <inge_dewaard@hotmail.com>
2020-03-20 10:23:04 +0100

    Commit eslintrc.json TECH

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,35 @@
+// Const en Requires
+
 const express = require('express')
 const app = express()
-const port = 3000
-// body-parser laat server weten wat voor data er binnen komt
-const bodyParser = require('body-parser')
+const port = 3000                                     // Localhost:3000
+const bodyParser = require('body-parser')             // Body-parser laat server weten wat voor data er binnen komt
 const multer  = require('multer')
-//slug maakt url save (verandert leestekens goed)
-const slug = require('slug')
+const slug = require('slug')                          // Slug maakt url save (verandert leestekens goed)
 const session = require('express-session')
 const upload = multer({dest:'static/upload/'})
-require('dotenv').config()
-//Connect met de database van Mongo
-var db;
+require('dotenv').config()                            // Zodat je process.env.DB_USER etc. kunt gebruiken
+
+
+// Database MongoDB
+
+var db;                                               // Connect met de database van Mongo
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@projecttech-hvfp2.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(url, { useNewUrlParser: true });
   client.connect((err, client, res) => {
     if (err) {
-      console.log(err);
+      console.log(err)
     }
     db = client.db(process.env.DB_NAME);
   });
 
-// Eerst alle app.set en app.use
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-// app.use('/static', express.static(__dirname + '/static));
-app.use(express.static('static'))
+
+// SET en USE
+
+app.set('view engine', 'ejs')                         // Template engine EJS
+app.set('views', 'views')                             // Alle EJS bestanden in de map views
+app.use(express.static('static'))                     // Andere mogelijkheid voor kortere linkjes: app.use('/static', express.static(__dirname + '/static));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(session({
@@ -34,39 +38,57 @@ app.use(session({
     secret: process.env.SESSION_SECRET
   }))
 
-// Dan alle app.get en app.post (get stuurt iets terug naar client)
-// Routing
+// GET: (get stuurt url naar client na aanvraag naar de server)
+
 app.get('/', (req, res) => res.render('index.ejs', {title:'Maak een profiel aan'}))
 app.get('/error', (req, res) => res.status(404).render('index.ejs', {title:'Panieek'}))
-app.get('/about', (req, res) => res.send('Over mij'))
-app.get('/file', (req, res) => res.sendFile('/Users/inge/Documents/• HVA/JAAR 2/TECH/datingapp/static/images/banner6.jpg'))
-app.get('/mp3', (req, res) => res.sendFile('/Users/inge/Documents/• HVA/JAAR 2/TECH/datingapp/static/images/mpvierrr.mp4'))
 app.get('/profile/:id', profile)
+app.get('*', (req, res) => res.send('404 error not found'))         // Als je op een route komt die ik niet gedefinieerd heb, laat hij een error zien
 
-  function profile(req, res) {
-        const id = req.params.id
-        res.render('result.ejs', req.session.user);
-      }
+// function profile(req, res, err) {
+//   const id = req.params.id
+//   if(!req.session.user){
+//     res.render('result.ejs', req.session.user)
+//   } else {
+//     return err ('Vul alle velden in om een juist profiel op te zetten.')
+//     console.log(err)
+//   }
+// }
+
+function profile(req, res, err) {
+  // const id = req.params.id
+  if (!req.session.user) {
+    // req.session.user = {username: user.username};
+    // res.redirect('/')
+    res.send('Vul alle velden in om een juist profiel op te zetten.')
+  } else {
+    res.render('result.ejs', req.session.user)
+  }
+}
+
+
+
+
+
+// POST: (client post data in de database via de server)
 
 app.post('/add-profile', upload.single('cover'), myForm)
-app.post('/', upload.single('cover'), myForm)
 
-  function myForm(req, res, next){
-    const id = slug(req.body.username).toLowerCase()
-    req.session.user = {
-  		  username: req.body.username,
-  		  regio: req.body.regio,
-  		  jaar: req.body.jaar,
-        gender:req.body.gender,
-        biografie: req.body.biografie,
-        cover: req.file ? req.file.filename : null
-  	   };
-       db.collection('profiles').insertOne(req.session.user)
-       console.log(req.session.user)
-       res.redirect('profile/' + req.body.username);
-     }
+function myForm(req, res){
+  const id = slug(req.body.username).toLowerCase()
+  req.session.user = {
+	  username: req.body.username,
+	  regio: req.body.regio,
+	  jaar: req.body.jaar,
+    gender:req.body.gender,
+    biografie: req.body.biografie,
+    cover: req.file ? req.file.filename : null
+  }
+    db.collection('profiles').insertOne(req.session.user) // Profiles is een map in de database waar req.session.user geinsered wordt
+    // db.collection('profiles').update(req.session.user)
+    console.log(req.session.user)                         // Hier ergens update functie/method toevoegen om data up te daten in de database
+    res.redirect('profile/' + req.body.username)
+}
 
-// als je op een route komt die ik niet gedefinieerd heb, laat hij een error zien
-app.get('*', (req, res) => res.send('404 error not found'))
 // listen altijd als laatste
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
