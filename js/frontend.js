797d088692b98167698c5b41fbba01bd9e5a7e19
IngeDW <inge_dewaard@hotmail.com>
2020-04-12 12:30:40 +0200

    Add consistancy and progressive enhancement

--- js/frontend.js
+++ js/frontend.js
@@ -1,7 +1,3 @@
-// Code standards
-// • Always use ;
-// • String 'between single quote'
-
 // Retrieves tags to use in JavaScript
 const introduction = document.querySelector('[data-introduction]');
 const buttonIntro = document.querySelector('[href="#scrollToForm"]');
@@ -9,19 +5,34 @@ const profileForm = document.querySelector('[data-profileForm]');
 
 
 // Positioning the form NOT in sight
-profileForm.classList.add('weg');
+profileForm.classList.add('gone');
 
 
 // Function that changes the intro text to a form where the user can make a profile
 buttonIntro.addEventListener('click', function(){
     event.preventDefault();                                      // Don't do default
-    profileForm.classList.toggle('tevoorschijn');                // Show form
-    introduction.classList.add('wegIntro');                      // Remove intro text
+    profileForm.classList.toggle('showForm');                    // Show form
+    introduction.classList.add('introGone');                     // Remove intro text
 });
 
+// This code is to show how CONTEXT works and doen't apply on my feature
+const profile = {
+    name: 'Inge',
+    age: 22,
+    place: 'Egmond',
+    message: function(){
+        return `${this.name} is ${this.age} years old and lives in ${this.place}!`;
+    },
+};
 
-// Object with input data from user
-let inputUser = document.querySelector('[name="gebruikersnaam"]').value;
+console.log(profile.message());
 
+// This code is to show how CLOSURE works and doen't apply on my feature
+let addTo = function(outer){
+  let add= function(inner){
+    return outer + inner;
+  };
+  return add;
+};
 
-console.log(inputUser);
+console.log(addTo(2)(3));
